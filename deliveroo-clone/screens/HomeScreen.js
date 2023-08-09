558c8a4a1610c4b621bfd2802613f2a8a39c4137
steves3142 <steves3142@gmail.com>
2023-08-08 23:57:43 -0400

    setting up

---
+++ deliveroo-clone/screens/HomeScreen.js
@@ -0,0 +1,37 @@
+import { View, Text, Image } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { useNavigation } from '@react-navigation/native'
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function HomeScreen() {
+    const navigation = useNavigation();
+
+    useLayoutEffect(() => {
+            navigation.setOptions({
+                headerShown: false,
+            });
+
+    }, []); 
+
+  return (
+    <SafeAreaView>
+      <Text className="text-red-500">
+        
+        {/* Header */}
+        <View className="flex-row pb-3 items-center mx-4 space-x-2">
+            <Image 
+                source={{
+                    uri: "https://links.papareact.com/wru"
+                }}
+                className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+            />
+            <View>
+                <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+                <Text className="font-bold text-xl">Current Location</Text>
+            </View>
+        </View>
+
+      </Text>
+    </SafeAreaView>
+  )
+}
