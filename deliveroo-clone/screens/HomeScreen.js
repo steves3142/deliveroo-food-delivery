0eb2dc0028af60ee71f94b761d215422b3e240a0
steves3142 <steves3142@gmail.com>
2023-08-11 01:38:20 -0400

    adding search bar

--- deliveroo-clone/screens/HomeScreen.js
+++ deliveroo-clone/screens/HomeScreen.js
@@ -1,6 +1,12 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, TextInput } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
+import {
+  UserIcon,
+  ChevronDownIcon,
+  SearchIcon,
+  AdjustmentsIcon,
+} from "react-native-heroicons/outline";
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function HomeScreen() {
@@ -25,12 +31,31 @@ export default function HomeScreen() {
                 }}
                 className="h-7 w-7 bg-gray-300 p-4 rounded-full"
             />
-            <View>
-                <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
-                <Text className="font-bold text-xl">Current Location</Text>
+            <View className="flex-1">
+                <Text className="font-bold text-gray-400 text-xs">
+                  Deliver Now!
+                </Text>
+                <Text className="font-bold text-xl">
+                  Current Location
+                  <ChevronDownIcon size={20} color="#00CCBB" />
+                </Text>
             </View>
+
+            <UserIcon size={35} color="#00CCBB"/>
         </View>
 
+        {/* Search */}
+        <View>
+          <View>
+            <SearchIcon color="gray" size={20}/>
+            <TextInput
+            placeholder="Restaurants and cuisines"
+            keyboardType="default"
+          />
+        </View>
+
+        {/* <AdjustmentsIcon color="#00CCBB" /> */}
+        </View>
       </Text>
     </SafeAreaView>
   )
