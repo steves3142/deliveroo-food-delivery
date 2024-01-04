2da1f0601867ac257f4ceffd2f2ca77686e6e9a2
steves3142 <steves3142@gmail.com>
2024-01-04 12:46:53 -0500

    adding changes to RestaurantCard

--- deliveroo-clone/components/RestaurantCard.js
+++ deliveroo-clone/components/RestaurantCard.js
@@ -1,6 +1,6 @@
 import { View, Text, TouchableOpacity, Image} from 'react-native'
 import React from 'react'
-import { StarIcon } from 'react-native-heroicons/outline'
+import { LocationMarkerIcon, StarIcon } from 'react-native-heroicons/outline'
 
 const RestaurantCard = ({
     id,
@@ -15,7 +15,7 @@ const RestaurantCard = ({
     lat,
 }) => {
   return (
-    <TouchableOpacity>
+    <TouchableOpacity className='bg-white mr-3 shadow'>
       <Image
         source={{
             uri: imgUrl,
@@ -25,10 +25,15 @@ const RestaurantCard = ({
         <View className="px-3 pb-4">
             <Text className='font-bold text-lg pt-2'>{title}</Text>
             <View className='flex-row items-center space-x-1'>
-                <StarIcon color="green" opacity={0.5} size={22} />
+                <StarIcon color="green" opacity={0.9} size={22} />
                 <Text className="text-xs text-gray-500">
-                    <Text className="text-green-500">{rating}</Text> . {genre}
+                    <Text className="text-green-500">{rating}</Text> · {genre}
                 </Text>
+            </View>
+
+            <View className='flex-row items-center space-x-1'>
+              <LocationMarkerIcon color="gray" opacity={0.4} size={22} />
+              <Text className="text-xs text-gray-500">Nearby · {address}</Text>
             </View>
         </View>
     </TouchableOpacity>
